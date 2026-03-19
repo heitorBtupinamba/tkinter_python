f5af4045f97a08f5c57d540c1ed997f06db676b2
Heitor Antonio Botelho Tupinamba <951131@senacminas.edu.br>
2026-03-19 09:43:32 -0300

    Formulários de Cadastro com TkInter

---
+++ formulario_cadastro.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkinter import messagebox
+
+janela = tk.Tk()
+janela.title("Cadastro")
+janela.geometry("300x300")
+
+def enviar():
+    nome = entrada_nome.get()
+    idade = entrada_idade.get()
+    escolaridade = combo_escolaridade.get()
+    area_atuacao = radio_area_atuacao.get()
+
+    if area_atuacao == 1:
+        area_atuacao = "TI"
+    elif area_atuacao == 2:
+        area_atuacao = "Vendas"
+    elif area_atuacao == 3:
+        area_atuacao = "RH"
+    elif area_atuacao == 4:
+        area_atuacao = "Marketing"
+
+    msg = f"Nome: {nome}\n Idade: {idade}\n Escolaridade: {escolaridade}\n Área de Atuação: {area_atuacao}"
+    messagebox.showinfo("Dados enviados com sucesso!", msg)
+
+tk.Label(janela, text="Formulário de Cadastro", font=("Arial",20)).grid(column=1, pady = 20)
+
+#entrada de texto - Nome
+tk.Label(janela, text="Nome completo:",  font=("Arial")).grid(row=1, column=0)
+entrada_nome= tk.Entry(janela,font=("Arial"))
+entrada_nome.grid(row=1, column=1)
+
+#entrada de texto - Idade
+tk.Label(janela, text="Idade:",  font=("Arial")).grid(row=3, column=0)
+entrada_idade= tk.Entry(janela,font=("Arial"))
+entrada_idade.grid(row=3, column=1)
+
+#COMBOBOX
+tk.Label(janela,text="Escolaridade:",font="Arial").grid(row=4, column=0)
+combo_escolaridade = ttk.Combobox(janela,font="Arial", values=["Ensino Médio","Ensino Técnico","Ensino-Superior","Pós-Graduação"])
+combo_escolaridade.grid(row=4, column=1)
+
+#radioButton
+radio_area_atuacao = tk.IntVar()
+tk.Label(janela, text="Área de Atuação:",font=("Arial")).grid(row=5,column=0)
+tk.Radiobutton(janela, text="TI", font=("Arial"), value=1,variable=radio_area_atuacao).grid(row=5,column=1)
+tk.Radiobutton(janela, text="Vendas", font=("Arial"), value=2,variable=radio_area_atuacao).grid(row=6,column=1)
+tk.Radiobutton(janela, text="RH", font=("Arial"), value=3,variable=radio_area_atuacao).grid(row=7,column=1)
+tk.Radiobutton(janela, text="Marketing", font=("Arial"), value=4,variable=radio_area_atuacao).grid(row=8,column=1)
+
+#Botão
+tk.Button(janela, text="Enviar",command=enviar).grid(row=9, column=1, pady=20)
+
+
+
+
+janela.mainloop()
--- grid_form.py
+++ grid_form.py
@@ -4,22 +4,44 @@ from tkinter import *
 from tkinter import messagebox
 
 janela = tk.Tk()
-janela.title("Formulario")
+janela.title("Formulário")
 janela.geometry("300x300")
+
+def enviar():
+    nome = entrada_nome.get()
+    estado = combo_estado.get()
+    sexo = opc.get()
+
+    if sexo == 1:
+        sexo_texto = "Masculino"
+    else:
+        sexo_texto = "Feminino"
+
+    msg = f"Nome: {nome}\n Estado: {estado}\n Sexo: {sexo_texto}"
+    messagebox.showinfo("Dados Enviados", msg)
+
+tk.Label(janela, text="Formulário de Cadastro",font=("Arial",20)).grid(column=1, pady=20)
+
 #entrada de texto
-tk.Label(janela, text="Nome:",  font=("Arial")).grid(row=0, column=0)
-entrada_nome= tk.Entry(janela,font=("Papyrus"))
-entrada_nome.grid(row=0, column=1)
+tk.Label(janela, text="Nome:",  font=("Arial")).grid(row=1, column=0)
+entrada_nome= tk.Entry(janela,font=("Arial"))
+entrada_nome.grid(row=1, column=1)
 #radiobutton
 opc = tk.IntVar()
 
-tk.Label(janela, text="Sexo:", font=("Arial")).grid(row=2, column=0)
-frame_sx = tk.Frame(janela)
-frame_sx.grid(row=3, column=1, padx=10, pady=5, sticky="w")
-tk.Radiobutton(frame_sx, text="Masculino", font=("Papyrus") ,value=1, variable=opc)\
-    .pack(anchor="w")
-tk.Radiobutton(frame_sx, text="Feminino", font=("Papyrus") ,value=2, variable=opc)\
-    .pack(anchor="w")
+#radioButton
+opc = tk.IntVar()
+tk.Label(janela, text="Sexo:",font=("Arial")).grid(row=2,column=0)
+tk.Radiobutton(janela, text="Masculino", font=("Arial"), value=1,variable=opc).grid(row=3,column=1)
+tk.Radiobutton(janela, text="Feminino", font=("Arial"), value=2,variable=opc).grid(row=4,column=1)
+
+#COMBOBOX
+tk.Label(janela,text="Estado").grid(row=5, column=0)
+combo_estado = ttk.Combobox(janela, values=["MG","SP","RJ","RN","BA"])
+combo_estado.grid(row=5, column=1)
+
+#Botão
+tk.Button(janela, text="Enviar",command=enviar).grid(row=7, column=1, pady=20)
 
 
 janela.mainloop()
